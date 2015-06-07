1f564b8641daff86459c0b73897a9449ec5c8726
Nycto <James@RoundEights.com>
2015-06-07 16:09:35 -0700

    Basic implementation

--- astar.nim
+++ astar.nim
@@ -3,11 +3,20 @@
 ## @see http://www.redblobgames.com/pathfinding/a-star/introduction.html
 ##
 
-import binaryheap, tables
+import binaryheap, tables, hashes
 
 type
     Distance* = int ## \
         ## Distance is used to measure cost and the heuristic between two points
+
+    Node* = concept n ## \
+        ## Represents a node stored within a graph
+
+        `==`(n, n) is bool ## \
+            ## Nodes must be comparable so we know when we reach the goal
+
+        `hash`(n) is THash ## \
+            ## Nodes are used as keys in a table, so they need to be hashable
 
     Graph*[N] = concept g ## \
         ## The graph being traversed
@@ -30,7 +39,7 @@ type
             ## Estimates the distance between two nodes in the graph
 
 
-proc newAStar*[G, N](
+proc newAStar*[G: Graph, N: Node](
     graph: G,
     heuristic: proc(a, b: N): Distance {.closure.}
 ): AStar[G, N] =
@@ -39,36 +48,75 @@ proc newAStar*[G, N](
 
 
 type
-    FrontierElem[N] = tuple[node: N, cost: Distance] ## \
+    FrontierElem[N] = tuple[node: N, priority: Distance] ## \
         ## Internally used to associate a graph node with how much it costs
 
-    CameFrom[N] = distinct tuple[node: N, cost: Distance] ## \
+    CameFrom[N] = tuple[node: N, cost: Distance] ## \
         ## Given a node, this stores the node you need to backtrack to to get
-        ## to this node and how much it costs to get here.  Yes, I am aware
-        ## this has the same structure as FrontierElem, but they are
-        ## conceptually different; hence different definitions.
+        ## to this node and how much it costs to get here
 
-iterator path*[G: Graph, N]( graph: AStar[G, N], start, goal: N ): N =
+
+iterator backtrack[N]( cameFrom: Table[N, CameFrom[N]], start, goal: N ): N =
+    ## Once the table of back-references is filled, this yields the reversed
+    ## path back to the consumer
+    yield start
+
+    var current: N = goal
+    var path: seq[N] = @[]
+
+    while current != start:
+        path.add(current)
+        current = `[]`(cameFrom, current).node
+
+    if path.len > 0:
+        for i in (path.len - 1)..0:
+            yield path[i]
+
+iterator path*[G: Graph, N: Node]( astar: AStar[G, N], start, goal: N ): N =
     ## Iterates over the nodes that connect the start and goal
 
-    if start != goal:
+    # The frontier is the list of nodes we need to visit, sorted by a
+    # combination of cost and how far we estimate them to be from the goal
+    var frontier =
+        newHeap[FrontierElem[N]] do (a, b: FrontierElem[N]) -> Distance:
+            return a.priority - b.priority
 
-        # The frontier is the list of nodes we need to visit, sorted by a
-        # combination of cost and how far we estimate them to be from the goal
-        var frontier = newHeap[FrontierElem[N]](
-            proc (a, b: FrontierElem[N]): Distance =
-                let aCost: Distance = a.cost
-                let bCost: Distance = b.cost
-                return aCost - bCost
-        )
+    # Put the start node into the frontier so we have a place to kick off
+    frontier.push( (node: start, priority: 0) )
 
-        # Put the start node into the frontier so we have a place to kick off
-        frontier.push( (node: start, cost: 0) )
+    # A map of backreferences. After getting to the goal, you use this to walk
+    # backwards through the path and ultimately find the reverse path
+    var cameFrom = initTable[N, CameFrom[N]]()
 
-        # A map of backreferences. After getting to the goal, you use this to walk
-        # backwards through the path and ultimately find the reverse path
-        var cameFrom = newTable[N, CameFrom[N]]()
+    while frontier.size > 0:
+        let current = frontier.pop
 
-        while frontier.size > 0:
-            let current = frontier.pop
+        # Once we reach the goalpost, we're set.
+        if current.node == goal:
+            break
+
+        let currentCost = `[]`(cameFrom, current.node).cost
+
+        for next in astar.graph.neighbors(current.node):
+
+            # Adding current cost lets us track the total it took to get here
+            let cost = currentCost + astar.graph.cost(current.node, next)
+
+            # If we haven't seen this point already, or we found a cheaper
+            # way to get to that
+            if not cameFrom.hasKey(next) or cost < `[]`(cameFrom, next).cost:
+
+                # Add this node to the backtrack map
+                `[]=`(cameFrom, next, (node: current.node, cost: cost))
+
+                # Also add it to the frontier so we check out its neighbors
+                frontier.push((
+                    node: next,
+                    priority: cost + astar.heuristic(next, goal)
+                ))
+
+    # Now that we have a map of back-references, yield the path back out to
+    # the caller
+    for node in backtrack(cameFrom, start, goal):
+        yield node
 
--- test/astar_test.nim
+++ test/astar_test.nim
@@ -1,4 +1,4 @@
-import astar, unittest, sets, sequtils
+import astar, unittest, sets, sequtils, strutils, ropes
 
 type
     Grid = object
@@ -6,29 +6,95 @@ type
 
     XY = tuple[x, y: int]
 
-proc grid( rows: varargs[string] ): Grid =
-    discard
+proc grid( ascii: varargs[string] ): Grid =
+    ## Creates a Grid from ascii art strings
+    var rows: seq[seq[int]] = @[]
+    for asciiRow in ascii:
+        var row: seq[int] = @[]
+        for point in asciiRow:
+            case point
+            of ' ': discard
+            of '.': row.add(0)
+            of '0'..'9': row.add(parseInt($point))
+            else: row.add(-1)
+        rows.add(row)
+    return Grid(rows: rows)
 
-proc cost( grid: Grid, a, b: XY ): int = 0
+proc cost( grid: Grid, a, b: XY ): int =
+    ## Returns the cost associated with moving to a point
+    return grid.rows[b.y][b.x]
 
-iterator neighbors( grid: Grid, point: XY ): XY =
-    discard
+proc isValid( grid: Grid, point: XY ): bool =
+    ## Returns whether a point exists in this grid
+
+iterator neighbors*( grid: Grid, point: XY ): XY =
+    ## Yields the connected neighbors of a point
+    let adjacent = [
+        (x: point.x - 1, y: point.y),
+        (x: point.x + 1, y: point.y),
+        (x: point.x, y: point.y - 1),
+        (x: point.x, y: point.y + 1)
+    ]
+    for adj in adjacent:
+        if adj.y >= 0 and adj.y < grid.rows.len:
+            if adj.x >= 0 and adj.x < grid.rows[adj.y].len:
+                if grid.rows[adj.y][adj.x] >= 0:
+                    yield adj
+
+proc `$`( grid: Grid ): string =
+    ## Converts a grid to a string
+    var str = rope("")
+    for row in grid.rows:
+        if str.len != 0:
+            str.add("\n")
+        for column in row:
+            if column < 0:
+                str.add("#")
+            elif column == 0:
+                str.add(".")
+            else:
+                str.add($column)
+            str.add(" ")
+    return $str
+
+proc `$`( point: XY ): string =
+    ## Converts a point to a readable string
+    return "(" & $point.x & ", " & $point.y & ")"
+
+proc createAStar( grid: Grid ): AStar[Grid, XY] =
+    ## Creates an AStar instance prefilled to use a manhatten distance heuristic
+    return newAStar[Grid, XY](grid) do (a, b: XY) -> int:
+        return abs(a.x - b.x) + abs(a.y - b.y)
+
+proc assert( within: Grid, starting: XY, to: XY, equals: openArray[XY] ) =
+    ## Asserts a path is created across the given grid
+    let astar = createAStar(within)
+    let path = toSeq( path[Grid, XY](astar, starting, to) )
+    checkpoint("Grid is:\n" & $within)
+    checkpoint("Expected: " & join(map(equals, `$`), " -> "))
+    checkpoint("Actual:   " & join(map(path, `$`), " -> "))
+    assert( path == @equals )
 
 
 suite "A* should":
 
-    const square = grid(
-        ".....",
-        ".....",
-        ".....",
-        ".....",
-        "....."
-    )
+    test "Yield a single point when goal == start":
+        assert(
+            grid(
+                ". . .",
+                ". . .",
+                ". . ."),
+            starting = (0, 0), to = (0, 0),
+            equals = [(0, 0)]
+        )
 
-    test "Yield nothing when goal == start":
-        let astar = newAStar[Grid, XY](square) do (a, b: XY) -> int:
-            return abs(a.x - b.x) + abs(a.y - b.y)
-
-        let path = toSeq( path[Grid, XY](astar, (0, 0), (0, 0)) )
-        require( path == @[] )
+    test "Yield two points for connected points":
+        assert(
+            grid(
+                ". . .",
+                ". . .",
+                ". . ."),
+            starting = (0, 0), to = (1, 0),
+            equals = [ (0, 0), (1, 0) ]
+        )
 
